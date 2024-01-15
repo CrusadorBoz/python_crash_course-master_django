e3cfbb9143a93b0f2aa5e4cbd9fd33e3e17c6391
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-01-15 08:36:07 -0600

    Update

--- brain/settings.py
+++ brain/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My Apps
+    'master',
+    # Default Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # My Apps
-    'master',
-
+    
 ]
 
 MIDDLEWARE = [
