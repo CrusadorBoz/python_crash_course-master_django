440f799e4d51de5b697369310eba34cd12e856a5
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-01-23 06:12:07 -0600

    saving tree

--- master/views.py
+++ master/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -61,3 +61,22 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {'topic': topic, 'form': form}
     return render(request, 'master/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """ Edit an existing entry. """
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('master:topic', topic_id=topic.id)
+    # Display a blank or invalid form.
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'master/edit_entry.html', context)
